7ebda86856bfe2f67707152cb3190911cd95f206
Ahmed\Ahmed Yehia <ahmedyehiakhater@gmail.com>
2018-06-01 09:04:55 +0200

    Bug with html loading before js

--- PU-FE/src/pages/home/home.ts
+++ PU-FE/src/pages/home/home.ts
@@ -6,9 +6,28 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public user: {
+    userFullName: string;
+    userID: number;
+    userEmail: any;
+    achievedCredits: number;
+    registeredCredits: number;
+    cummulativeGpa: number;
+    semesterGpa: number;
+    ukGpa: number;
+  };
   constructor(public navCtrl: NavController) {
-
+    this.user = {
+    userFullName : "Ahmed Yehia Khater",
+    userID : 136471,
+    userEmail : "ahmedyehiakhater@gmail.com",
+    achievedCredits : 136,
+    registeredCredits : 18,
+    cummulativeGpa : 2.9,
+    semesterGpa : 2.5,
+    ukGpa : 2.7
+    };
+    console.log(this.user);
   }
 
 }
